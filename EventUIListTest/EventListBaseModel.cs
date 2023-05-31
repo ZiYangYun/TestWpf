93920ed4ac0002486b6b603bc30f21ef99b1c5e2
@ziyang.yan <3540863389@qq.com>
2023-05-31 18:35:42 +0800

    资源调用写了一半

---
+++ EventUIListTest/EventListBaseModel.cs
@@ -0,0 +1,71 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventUIListTest
+{
+    public class EventListBaseModel: ViewModelBase,IDisposable
+    {
+        public UINodeTypeEnum NodeType;
+
+        private string _Color = "#333333";
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                if (_Color == value)
+                {
+                    return;
+                }
+                _Color = value;
+                RaisePropertyChanged("Color");
+            }
+        }
+
+        private string _BackgroundColor = "Transparent";
+        public string BackgroundColor
+        {
+            get
+            {
+                return _BackgroundColor;
+            }
+            set
+            {
+                if (_BackgroundColor == value)
+                {
+                    return;
+                }
+                _BackgroundColor = value;
+                RaisePropertyChanged("BackgroundColor");
+            }
+        }
+        private string _NodeValue;
+        public string NodeValue
+        {
+            get
+            {
+                return _NodeValue;
+            }
+            set
+            {
+                if (_NodeValue == value)
+                {
+                    return;
+                }
+                _NodeValue = value;
+                RaisePropertyChanged("NodeValue");
+            }
+        }
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ EventUIListTest/EventListDataTemplateSelector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EventUIListTest
+{
+    public class EventListDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultnDataTemplate { get; set; }
+
+        public DataTemplate PartDescDataTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is EventListBaseModel eventListBaseModel)
+            {
+                switch (eventListBaseModel.NodeType)
+                {
+                    case UINodeTypeEnum.none:
+                        return DefaultnDataTemplate;
+                    case UINodeTypeEnum.question:
+                        return PartDescDataTemplate;
+                    default:
+                        break;
+                }
+            }
+            return DefaultnDataTemplate;
+        }
+    }
+
+    public enum UINodeTypeEnum
+    {
+        /// <summary>
+        /// 默认为无
+        /// </summary>
+        none = 0,
+
+        /// <summary>
+        /// 大题节点, 表示一道完整的大题
+        /// </summary>
+        question = 1,
+    }
+}
